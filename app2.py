0ae092464f73d3f3fc2c3b9f7e6e2f03b06a1874
rsharvesh16 <rsharvesh16@gmail.com>
2024-01-24 23:28:48 +0530

    Files Added

--- app2.py
+++ app2.py
@@ -7,8 +7,8 @@ import numpy as np
 import pytesseract 
 import pyttsx3
 
-
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 def ocr(image):
     text=pytesseract.image_to_string(image, lang = 'eng')
     print("Recognized Text:", repr(text))
@@ -23,26 +23,51 @@ def speak_text(text):
     engine.setProperty('voice', voices[1].id)
     engine.say(text)
     engine.runAndWait()
+    
 
 def get_image_download_link(image):
     buffered = io.BytesIO()
     image.save(buffered, format="PNG")
     image_str = base64.b64encode(buffered.getvalue()).decode()
-    href = f'<a href="data:file/png;base64,{image_str}" download="signed_image.png">Download Text</a>'
-    return href
-
+    with open('test.jpg', 'wb') as image_file:
+        image_file.write(base64.urlsafe_b64decode(image_str))
+    return ""
 
 def main():
     # Apply a light background color to the entire app
-    theme = """
+    # background_image = Image.open("crop.jpg")
+    # st.image(background_image, use_column_width=True)
+
+    # theme = f"""
+    # <style>
+    #     body {{
+    #         background: url(data:image/jpeg;base64,{base64.b64encode(background_image.tobytes()).decode()});
+    #         background-size: cover;
+    #         color: #333; /* Light mode text color */
+    #     }}
+    # </style>
+    # """
+    # st.markdown(theme, unsafe_allow_html=True)
+
+    page_bg_img = '''
     <style>
-        body {
-            background-color: #f0f0f0; /* Light mode background color */
-            color: #333; /* Light mode text color */
-        }
+    [data-testid="stAppViewContainer"]{
+    background-image: url("https://wallpapercave.com/wp/wp9163623.jpg");
+    background-size: cover;
+    opacity: 0.8;
+    }
+
+    [data-testid="stHeader"]{
+    background-color: rgba(0,0,0,0);
+    }
+
+    [date-testid="stToolbar"]{
+    right =
+    }
     </style>
-    """
-    st.markdown(theme, unsafe_allow_html=True)
+    '''
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
 
     st.title("HandScribeVoice")
 
@@ -68,23 +93,18 @@ def main():
         Text_image = Image.fromarray(image_array.astype('uint8'), 'RGBA')
 
         st.image(Text_image, caption="Text", use_column_width=True)
-
         # Save the image with the drawn Text
         st.markdown(get_image_download_link(Text_image), unsafe_allow_html=True)
-    
-    uploaded_file = st.file_uploader("Choose an image...", type="png")
+        
+    # Display the uploaded image
+    image = Image.open("test.jpg")
 
-    if uploaded_file is not None:
-        # Display the uploaded image
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+    # Perform OCR on the image
+    text_result = ocr(image)
 
-        # Perform OCR on the image
-        text_result = ocr(image)
-
-        # Display the recognized text
-        st.header("Recognized Text:")
-        st.text(text_result)
+    # Display the recognized text
+    st.header("Recognized Text:")
+    st.text(text_result)
 
     st.warning("Click the button below to listen to the recognized text.")
     if st.button("Listen to Text"):
